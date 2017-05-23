9705528476fd43ad1098ce876b1bfa0d94880582
hezanathos <hezanathos@gmail.com>
2017-05-23 16:12:22 +0200

    some quality improvment ( just the biginning... )

--- app/controllers/pirate_controller.rb
+++ app/controllers/pirate_controller.rb
@@ -9,10 +9,19 @@ class PirateController < ApplicationController
 
   def show
     @pirate = Pirate.find(params[:id])
-    keys_blacklist = %w(id created_at updated_at picpath) #these are the fields to hide
+    keys_blacklist = %w(id created_at updated_at) #these are the fields to hide
     @pirate_showlist = @pirate.attributes.except(*keys_blacklist)
-    @add_Points = @pirate.attributes["strengh"]+@pirate.attributes["intel"]+@pirate.attributes["wisdom"]!=@pirate.attributes["level"]*10
+    @addPoints = @pirate.attributes["strengh"]+@pirate.attributes["intel"]+@pirate.attributes["wisdom"]!=@pirate.attributes["level"]*10
+  end
 
+  def update
+    pirate = Pirate.find(params[:id])
+    pirate.update_attributes(:healthPoint=>1000/basePoints,:attackPoint=>basePoints,:intel=>0,:wisdom=>0,:strengh=>0,:level=>1)
+    if @pirate.save
+      redirect_to @pirate
+    else
+      render '/index'
+    end
   end
 
   def index
@@ -26,39 +35,16 @@ class PirateController < ApplicationController
   end
 
   def fight
-    @report = Array.new
-    i=0
     pirate1 = Pirate.find(params[:id1])
     pirate2 = Pirate.find(params[:id2])
-    hp1 = pirate1[:healthPoint]
-    hp2 = pirate2[:healthPoint]
+    @report = Pirate.reportBuilder pirate1, pirate2
 
-    dmg1=(pirate1[:attackPoint])
-    dmg2=(pirate2[:attackPoint])
-
-
-
-    while(hp1>0 && hp2>0)
-      @report.push "Il reste "+ hp1.to_s + "hp à "+pirate1[:name] +" et " + hp2.to_s + "hp à " + pirate2[:name]
-      @report.push "et paf, "+pirate1[:name]+" cogne "+pirate2[:name] + " et lui enlève " + dmg1.to_s + "hp"
-      @report.push "et vlan, "+pirate2[:name]+" cogne "+pirate1[:name] + " et lui enlève " + dmg2.to_s + "hp"
-      hp1 -=  dmg2
-      hp2 -=  dmg1
-      i= i+1
-      @report.push i.to_s
-    end
   end
 
-
-#<Pirate id: 1, name: "Alex", healthPoint: 124, intel: 0, strengh: 0, wisdom: 0, attackPoint: 876, created_at:
-# "2017-05-21 15:53:38", updated_at: "2017-05-21 15:53:38", email: "hezanathos@gmail.com", level: 1>
-
-
-
   def create
-    basePoints = rand(10)
+    basePoints = rand(10)+1
     @pirate = Pirate.new(pirate_params)
-    @pirate.update_attributes(:healthPoint=>basePoints,:attackPoint=>1000/basePoints,:intel=>0,:wisdom=>0,:strengh=>0,:level=>1)
+    @pirate.update_attributes(:healthPoint=>1000/basePoints,:attackPoint=>basePoints,:intel=>0,:wisdom=>0,:strengh=>0,:level=>1)
     if @pirate.save
       redirect_to @pirate
     else
@@ -70,13 +56,13 @@ class PirateController < ApplicationController
     params.require(:pirate).permit(:name, :email)
   end
 
-
   def edit
     @pirate = Pirate.find(params[:id])
   end
+
   def destroy
     @pirate = Pirate.find(params[:id]).destroy
-    flash[:success] = "Pirate supprimé"
+    flash[:success] = 'Pirate supprimé'
     redirect_to root_path
 
 
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,25 @@
 class Pirate < ActiveRecord::Base
+  def self.reportBuilder(pirate1,pirate2)
+    report =  Array.new
+    i=0
+
+    hp1 = pirate1[:healthPoint]
+    hp2 = pirate2[:healthPoint]
+
+    dmg1=(pirate1[:attackPoint])
+    dmg2=(pirate2[:attackPoint])
+    name1 = pirate1[:name]
+    name2 = pirate2[:name]
+
+    while(hp1>0 && hp2>0)
+      report.push "Il reste "+ hp1.to_s + "hp à "+name1 +" et " + hp2.to_s + "hp à " + name2
+      report.push "et paf, "+name1+" cogne "+name2 + " et lui enlève " + dmg1.to_s + "hp"
+      report.push "et vlan, "+name2+" cogne "+name1 + " et lui enlève " + dmg2.to_s + "hp"
+      hp1 -=  dmg2
+      hp2 -=  dmg1
+      i= i+1
+      report.push i.to_s
+    end
+  return report
+  end
 end
